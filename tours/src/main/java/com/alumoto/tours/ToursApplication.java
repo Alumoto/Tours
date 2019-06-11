a77816e51172e47d49670034af269342095c05a6
Alumoto <rniconicowww@gmail.com>
2019-06-11 18:18:55 +0900

    azure deploy fix

--- tours/src/main/java/com/alumoto/tours/ToursApplication.java
+++ tours/src/main/java/com/alumoto/tours/ToursApplication.java
@@ -3,7 +3,6 @@ package com.alumoto.tours;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 @EnableAutoConfiguration
