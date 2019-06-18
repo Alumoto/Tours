00a2f0cd91b2cfb509a6a1673ca6d4b891873b68
Alumoto <rniconicowww@gmail.com>
2019-06-18 18:40:50 +0900

    add logout

--- tours/src/main/java/com/alumoto/tours/form/UserForm.java
+++ tours/src/main/java/com/alumoto/tours/form/UserForm.java
@@ -2,16 +2,23 @@ package com.alumoto.tours.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 @Data
 public class UserForm {
 
-    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 20)
+    @Pattern(regexp = "[a-zA-Z0-9]+", message="すべて半角英数字で入力してください")
     String username;
 
-    @NotNull
+    @NotBlank
+    @Size(min = 5, max = 32)
+    @Pattern(regexp = "[a-zA-Z0-9]+", message="すべて半角英数字で入力してください")
     String password;
 
 }
